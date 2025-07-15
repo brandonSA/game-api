1e180c5b35963a83c699d5f280a7a052b9613fa6
Brandon Ho <brandon@manticorp.co.za>
2025-07-15 21:41:03 +0200

    Add swagger implementation

--- src/main/kotlin/manticorp/developer/game_api/GameController.kt
+++ src/main/kotlin/manticorp/developer/game_api/GameController.kt
@@ -1,12 +1,16 @@
 package manticorp.developer.game_api
 
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1")
 class GameController {
-    @GetMapping("/")
+
+    @Operation(summary = "Get game config", description = "Returns the game configuration")
+    @GetMapping("/game-config")
     fun listMessages() = listOf(
         Model("1", "Hello!"),
         Model("2", "Bonjour!"),
---
+++ src/main/kotlin/manticorp/developer/game_api/SwaggerConfig.kt
@@ -0,0 +1,21 @@
+package manticorp.developer.game_api
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Game API")
+                    .version("1.0.0")
+                    .description("This is a game API documented with Swagger and SpringDoc.")
+            )
+    }
+}
